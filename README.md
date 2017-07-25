f12854dcde8b083999964893fc2b65a789318ba8
jianl <jianjianfengyun@126.com>
2017-07-25 16:21:07 +0800

    i

---
+++ README.md
@@ -0,0 +1,11 @@
+# rc-mounter
+
+在任意地方挂载节点
+
+root 默认是 `document.body`
+
+```html
+
+<Mounter root={root}>children</Mounter>
+
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+import React, {PureComponent, cloneElement} from 'react'
+import {render} from 'react-dom'
+
+function validateDOMElem (props, propName, componentName){
+  const prop = props[propName];
+  if (prop != undefined && prop instanceof Element === false) {
+    return new Error(
+      'Invalid prop `' + propName + '` supplied to' +
+      ' `' + componentName + '`. Validation failed.'
+    );
+  }
+}
+
+export default class Mounter extends PureComponent {
+
+  static uiName = 'Mounter';
+
+  static propTypes = {
+    root: validateDOMElem
+  };
+
+  static defaultProps = {
+    root: document.body
+  }
+
+  container = document.createElement('div');
+
+  getContainer = ()=>{
+    return this.container;
+  };
+
+  destroy = ()=>{
+    this.container.remove();
+  };
+
+  mounted = ({children, ...rest})=>{
+    ReactDOM.render(<div {...rest}>{children}</div>, this.container);
+  }
+
+  componentDidMount(){
+    const {
+      root,
+      ...other
+    } = this.props;
+    root.appendChild(this.container);
+    this.mounted(other);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      root,
+      ...other
+    } = nextProps;
+    this.mounted(other);
+  }
+
+  render() {
+    return null;
+  }
+
+}
+
+
+Mounter.mount = function (component, props){
+  const container = document.createElement('div');
+  let rendered = render(cloneElement(component, props), container);
+  return {
+    getComponent: function(){
+      return rendered;
+    },
+    updateProps: function(props, callback){
+      rendered = render(cloneElement(component, Object.assign({}, rendered.props, props)), container, callback);
+    }
+  };
+};
